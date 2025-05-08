c4e28342d2acaaba3b75d81d945f9ab596a8bf90
Mihail-css <scooter-1982@yandex.ru>
2025-05-08 17:24:21 +0300

    fix js

--- src/js/index.js
+++ src/js/index.js
@@ -1,67 +1,22 @@
 
 // Мобильная навигация
- import mobileNav from './modules/mobile-nav.js';
- mobileNav();
-//avtoComplete поиск стран
-import autoComplete from "@tarekraafat/autocomplete.js";
-//список стран и стили
-import countryList from "./helpers/countryList";
-//календарь
-import { easepick } from '@easepick/bundle';
+import mobileNav from './modules/mobile-nav.js';
+mobileNav();
+
+import autoComplete from './modules/autoComplete.js';
+autoComplete();
+
+import easepick from './modules/easepick.js';
+easepick();
+
 //popular-slider
 import swiper from './modules/popular-slider.js';
 swiper();
+
 //scrollRevealFunc
 import scrollRevealFunc from './modules/scrollReveal.js';
 scrollRevealFunc();
+
 //testimonials-swiper
- import swiperTestimonials from './modules/swiper-testimonials.js';
- swiperTestimonials()
-//календарь
-const picker = new easepick.create({
-	element: document.getElementById('datePicket'),
-	css: [
-		'https://cdn.jsdelivr.net/npm/@easepick/bundle@1.2.1/dist/index.css',
-	],
-	zIndex: 10,
-	lang: "ru-RU",
-	format: "HH:mm, DD/MM/YY",
-	lang: "ru-RU",
-	TimePlugin: {
-		format12: false
-	 }
-	// plugins: [
-	// 	TimePlugin
-	// ]
-
-});
-
-//autoCompleteCountry
-const autoCompleteJS = new autoComplete({
-	selector: "#autoCompleteCountry",
-	placeHolder: "e.g Bali, Indonesia",
-	data: {
-		src: countryList,
-	},
-	resultItem: {
-		highlight: true,
-	},
-	events: {
-		input: {
-			selection: (event) => {
-				const selection = event.detail.selection.value;
-				autoCompleteJS.input.value = selection;
-
-			},
-
-		},
-	},
-	resultsList: {
-		maxResults: 10,
-	},
-	searchEngine: 'strict',
-
-});
-
-
-
+import swiperTestimonials from './modules/swiper-testimonials.js';
+swiperTestimonials();
